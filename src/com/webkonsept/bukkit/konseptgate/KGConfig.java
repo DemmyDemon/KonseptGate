56408e707d9eb06a761702a6d0f6733edab5d6f6
Fredrik Vold <fredrik@webkonsept.com>
2011-05-24 16:23:34 +0200

    The very basics of config handling

--- src/com/webkonsept/bukkit/konseptgate/KGConfig.java
+++ src/com/webkonsept/bukkit/konseptgate/KGConfig.java
@@ -1,9 +1,46 @@
 package com.webkonsept.bukkit.konseptgate;
 
+import java.io.File;
+import java.io.IOException;
+
 public class KGConfig {
-	KG plugin = null;
+	private KG plugin;
+	private File configFile;
+	public String configFileName;
 	
 	KGConfig(KG instance){
 		plugin = instance;
+		configFileName = plugin.getDataFolder().toString()+"/settings.yml";
+		configFile = new File(configFileName);
+		/*
+		
+		if (!createIfMissing(configFile)){
+			plugin.crap("Failed to create config file "+configFileName);
+		}
+		
+		*/
+	}
+	public boolean createIfMissing () {
+		boolean success = false;
+		if (configFile == null) return false;
+		
+		if (configFile.exists()){
+			success = true;
+		}
+		else {
+			if (configFile.mkdirs()){
+				try {
+					configFile.createNewFile();
+					success = true;
+				} catch (IOException e) {
+					e.printStackTrace();
+					success = false;
+				}
+			}
+			else {
+				success = false;
+			}
+		}
+		return success;
 	}
 }
